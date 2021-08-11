7aaaa34fe7187d2b98989ed25ef96ccabba8a078
Luã Mourão <luan.gcc@gmail.com>
2021-08-11 13:50:34 -0300

    Importa fonte personalizada e define estilização global básica

---
+++ src/styles/global.ts
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --body-bg: #0F0F0F;
+    --card-bg: #242424;
+    --blue: #3D6CB9;
+    --green: #17B978;
+  } 
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%;
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%;
+    }
+  }
+
+  body {
+    background: var(--body-bg);
+    -webkit-font-smoothing: antialiesed;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 500;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+`;
