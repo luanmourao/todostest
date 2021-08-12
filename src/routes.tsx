1a1071ac234822e9c81f9b0058a8273fec75a21a
Luã Mourão <luan.gcc@gmail.com>
2021-08-12 14:59:36 -0300

    Configura roteamento do menu navegável

---
+++ src/routes.tsx
@@ -0,0 +1,15 @@
+import { Switch, Route } from 'react-router-dom';
+import Todos from './pages/Todos';
+import Users from './pages/Users';
+
+const Routes = (): JSX.Element => {
+
+  return (
+    <Switch>
+      <Route path="/todos" exact component={Todos} />
+      <Route path="/users" component={Users} />
+    </Switch>
+  )
+};
+
+export default Routes;
